f69ee9bc9c4e44a9b35333f1f078c135e59066c9
Joseph Kimberger <joseph.kimberger@sky.uk>
2021-02-24 14:18:28 +0000

    Remove waitFor and use findBy instead

--- src/tests/features/searching.test.js
+++ src/tests/features/searching.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, waitFor } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
 import axios from "axios";
 
 import { renderWithRouter } from "../helpers";
@@ -6,38 +6,38 @@ import App from "../../App";
 
 describe("Searching for people", () => {
   it("renders results on partial match", async () => {
-    const { getByLabelText, getByText } = renderWithRouter(<App />);
+    const { getByLabelText, findByText } = renderWithRouter(<App />);
     fireEvent.change(getByLabelText("Character"), { target: { value: "r2" } });
-    await waitFor(() => getByText("R2-D2"));
+    await findByText("R2-D2");
   });
 
   it("allows users to paginate through results", async () => {
-    const { getByLabelText, getByText, findByText } = renderWithRouter(<App />);
+    const { getByLabelText, findByText } = renderWithRouter(<App />);
     fireEvent.change(getByLabelText("Character"), { target: { value: "r" } });
-    await waitFor(() => getByText("Luke Skywalker"));
+    await findByText("Luke Skywalker");
 
     fireEvent.click(await findByText("Next"));
-    await waitFor(() => getByText("Greedo"));
+    await findByText("Greedo");
 
     fireEvent.click(await findByText("Previous"));
-    await waitFor(() => getByText("Luke Skywalker"));
+    await findByText("Luke Skywalker");
   });
 
   it("debounces requests to the starwars api", async () => {
     const axiosSpy = jest.spyOn(axios, "get");
-    const { getByLabelText, getByText } = renderWithRouter(<App />);
+    const { getByLabelText, findByText } = renderWithRouter(<App />);
     fireEvent.change(getByLabelText("Character"), { target: { value: "x" } });
     fireEvent.change(getByLabelText("Character"), { target: { value: "r2" } });
-    await waitFor(() => getByText("R2-D2"));
+    await findByText("R2-D2");
     expect(axiosSpy).toHaveBeenCalledTimes(1);
     axiosSpy.mockRestore();
   });
 
   it("allows user to view character details", async () => {
-    const { getByLabelText, getByText, findByText } = renderWithRouter(<App />);
+    const { getByLabelText, findByText } = renderWithRouter(<App />);
     fireEvent.change(getByLabelText("Character"), { target: { value: "r2" } });
     fireEvent.click(await findByText("R2-D2"));
-    await waitFor(() => getByText("33BBY"));
+    await findByText("33BBY");
   });
 
   it("displays loading state while waiting for response", async () => {
--- src/tests/features/viewing_character.test.js
+++ src/tests/features/viewing_character.test.js
@@ -1,11 +1,10 @@
-import { waitFor } from "@testing-library/react";
 import { renderWithRouter } from "../helpers";
 import App from "../../App";
 
 describe("Viewing a character", () => {
   it("displays the films they have been in", async () => {
-    const { getByText } = renderWithRouter(<App />, { route: "/character/3" });
-    await waitFor(() => getByText("33BBY"));
-    await waitFor(() => getByText("A New Hope"));
+    const { findByText } = renderWithRouter(<App />, { route: "/character/3" });
+    await findByText("33BBY");
+    await findByText("A New Hope");
   });
 });
